91fb4376b599003d2fbe6c485e6b7bffe145d4a8
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2020-12-24 21:23:56 +0600

    High level languages are dominant in industries!

---
+++ helloworld.py
@@ -0,0 +1,34 @@
+#Just printing hello world
+print('Hello World')
+x = 10
+print(x)
+
+#Declaring a variable
+
+message = "Faizul Islam Fair"
+
+print(message)
+
+#Finding length of a string
+
+print(len(message))
+
+#Replacing Words
+
+word = "Allah is !"
+
+new_word = word.replace("!", "Most Gracious!")
+
+print(new_word)
+
+#Utilizing f strings
+
+greeting = "Hello"
+name = "Faizul"
+
+industry = f'{greeting}, {name.upper()}. Welcome!'
+
+print(industry)
+
+
+
