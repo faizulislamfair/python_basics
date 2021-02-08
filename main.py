5fba3e5261c3c3f43290a7f417527b1a2458ab6c
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-02-08 22:34:59 +0600

    First Python Project

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to my library! ")
+book = input("Which book do you want to buy? ")
+type = input("Which author do you like most? ")
+
+print("Yes", book, "is a really nice book,", type, "is also a fantastic author!")
+
