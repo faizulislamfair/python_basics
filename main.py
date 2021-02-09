b64d66c2797483f0be9ee60e0f6eefbf88454433
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-02-09 17:52:39 +0600

    Books

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 print("Welcome to my library! ")
 book = input("Which book do you want to buy? ")
 type = input("Which author do you like most? ")
+pricebook = input("Budget")
 
 print("Yes", book, "is a really nice book,", type, "is also a fantastic author!")
 
+if pricebook >= 500:
+    print("The book is too costly")
+
+    wants_to_play = input("Do you want to buy anyways? ")
+
+
+else:
+    print("Not sufficient budget.....")
