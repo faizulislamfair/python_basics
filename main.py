4ea3df81777fdf7451db9144b01be35218723459
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-02-10 13:24:59 +0600

    Just a random project

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
-print("Welcome to my library! ")
-book = input("Which book do you want to buy? ")
-type = input("Which author do you like most? ")
-pricebook = input("Budget")
+print("Welcome to this random game! ")
+name = input("What is your name? ")
+age = int(input("What is your age? "))
 
-print("Yes", book, "is a really nice book,", type, "is also a fantastic author!")
+print("Hello", name, "you are", age, "years old.")
 
-if pricebook >= 500:
-    print("The book is too costly")
+health = 10
 
-    wants_to_play = input("Do you want to buy anyways? ")
+if age >= 21:
+    print("You are old enough to play!")
 
+    wants_to_play = input("Do you want to play? ").lower()
+    if wants_to_play == "yes":
+        print("You are starting with", health, "health")
+        print("Let's play!")
+
+        left_or_right = input("First choice... Left or Right(left/right)? ")
+        if left_or_right == "left":
+            ans = input("Nice, you follow the path and reach a lake... Do you swim across or go around (across/around)? ")
+
+        if ans == "around":
+              print("You went around and reached the other side of the lake")
+
+        elif ans == "across":
+            print("You managed to get across, but were bit by a fish and lost 5 health.")
+            health -= 5
+
+        ans = input("You notice a house and a river. Which do you go to (river/house)? ")
+        if ans == "house":
+            print("You go to the house and are greeted by the owner... He doesn't like you and you lose 5 health")
+            health -= 5
+
+            if health <= 0:
+                print("You now have 0 health and you lost the game...")
+            else:
+                print("You have survived... You win!")
+
+
+        else:
+            print("You fell in the river and lost...")
+
+    else:
+            print("You fell down and lost...")
 
 else:
-    print("Not sufficient budget.....")
+        print("See yeah...")
+else:
+    print("You are not old enough to play...")
+
